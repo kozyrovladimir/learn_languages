d5c111192ef4c2770702bd04fdb2e9a023b5a45d
kozyrovladimir <kozyrovladimir@gmail.com>
2022-05-29 16:33:52 +0300

    feature: connect world reducer to main store, remove some comments

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,12 @@
 import {combineReducers, createStore} from 'redux';
+import {wordsReducer} from "./words-store";
 
-// объединяя reducer-ы с помощью combineReducers,
-// мы задаём структуру нашего единственного объекта-состояния
-const rootReducer = combineReducers({})
-// непосредственно создаём store
+const rootReducer = combineReducers({
+    words: wordsReducer,
+})
+
 export const store = createStore(rootReducer);
-// определить автоматически тип всего объекта состояния
 export type AppRootStateType = ReturnType<typeof rootReducer>
 
-// а это, чтобы можно было в консоли браузера обращаться к store в любой момент
 // @ts-ignore
-window.store = store
+window.store = store;
