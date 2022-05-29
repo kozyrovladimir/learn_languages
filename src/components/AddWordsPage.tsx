a0fcd60d6b9ab87bc11cfd8a7cfa88fa90a88950
kozyrovladimir <kozyrovladimir@gmail.com>
2022-05-29 13:03:48 +0300

    create: create empty AddWordsPage.tsx

---
+++ src/components/AddWordsPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddWordsPage = () => {
+    return (
+        <div>
+            <h2>AddWordsPage</h2>
+        </div>
+    );
+};
+
+export default AddWordsPage;
