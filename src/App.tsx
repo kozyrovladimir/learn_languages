dc0a38863fb83bb43d0fecfa07b2b9258dc1c7d1
kozyrovladimir <kozyrovladimir@gmail.com>
2022-05-29 13:41:17 +0300

    feature: add base features to header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <h1 style={{textAlign: 'center'}}>Language learn app</h1>
     </div>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import {Box, Tab, Tabs} from "@mui/material";
 
 const Header = () => {
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+        setValue(newValue);
+    };
+
     return (
-        <div>
-            <h2>Header</h2>
-        </div>
+        <Box sx={{ width: '100%' }}>
+            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+                    <Tab label="Добавить слова" />
+                    <Tab label="Изучать" />
+                </Tabs>
+            </Box>
+        </Box>
     );
 };
 
