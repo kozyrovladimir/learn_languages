4928fbdadbc5d684223478e1b931fe740401888e
kozyrovladimir <kozyrovladimir@gmail.com>
2022-06-17 17:59:58 +0300

    refactor: integrate formik for validation form inputs

--- src/components/AddWordForm.tsx
+++ src/components/AddWordForm.tsx
@@ -1,41 +1,64 @@
 import React from 'react';
 import {Box, Button, TextField} from "@mui/material";
+import * as yup from "yup";
+import {useFormik} from "formik";
+import {useDispatch} from "react-redux";
+import {addWordAC} from "../store/words-store";
 
-type AddWordFormType = {
-    rus: string,
-    eng: string,
-    errMessRus: string,
-    errMessEng: string,
-    rusInputOnChangeHandler: (e: React.ChangeEvent<HTMLInputElement>) => void,
-    engInputOnChangeHandler: (e: React.ChangeEvent<HTMLInputElement>) => void,
-    onSubmit: () => void
-}
+const AddWordForm = () => {
+    const dispatch = useDispatch();
 
-const AddWordForm = (props: AddWordFormType) => {
+    //using formik
+    const validationSchema = yup.object({
+        rus: yup.string()
+            .max(20, 'Слово не может быть больше 20 символов!')
+            .required('Поле обязательно!'),
+        eng: yup.string()
+            .max(20, 'Слово не может быть больше 20 символов!')
+            .required('Поле обязательно!'),
+    });
+
+    const formik = useFormik({
+        initialValues: {
+            rus: '',
+            eng: '',
+        },
+        validationSchema: validationSchema,
+        onSubmit: (values) => {
+            dispatch(addWordAC(values.rus, values.eng));
+            values.rus = '';
+            values.eng = '';
+            formik.touched.eng = false;
+            formik.touched.rus = false;
+        },
+    })
+
     return (
         <Box>
             <h3>Добавить новое слово для изучения:</h3>
             <Box sx={{paddingBottom: 2}}>
                 <TextField
-                    value={props.rus}
-                    onChange={props.rusInputOnChangeHandler}
-                    error={!!props.errMessRus}
-                    helperText={props.errMessRus}
-                    id="outlined-basic"
+                    value={formik.values.rus}
+                    onChange={formik.handleChange}
+                    error={formik.touched.eng && Boolean(formik.errors.rus)}
+                    helperText={formik.touched.rus && formik.errors.rus}
+                    id="rus"
+                    name="rus"
                     label="Слово на русском:"
                     variant="outlined"
                     sx={{marginRight: 2}}/>
                 <TextField
-                    value={props.eng}
-                    onChange={props.engInputOnChangeHandler}
-                    error={!!props.errMessEng}
-                    helperText={props.errMessEng}
-                    id="outlined-basic"
+                    value={formik.values.eng}
+                    onChange={formik.handleChange}
+                    error={formik.touched.eng && Boolean(formik.errors.eng)}
+                    helperText={formik.touched.eng && formik.errors.eng}
+                    id="eng"
+                    name="eng"
                     label="Слово на английском:"
                     variant="outlined"
                     sx={{marginRight: 2}}/>
                 <Button
-                    onClick={props.onSubmit}
+                    onClick={formik.submitForm}
                     variant={'outlined'}
                     size={'large'}>
                     Добавить
--- src/components/AddWordsPage.tsx
+++ src/components/AddWordsPage.tsx
@@ -2,37 +2,15 @@ import React from 'react';
 import AddWordForm from "./AddWordForm";
 import {Container} from "@mui/material";
 import WordTable from './WordTable';
-import {useDispatch, useSelector} from "react-redux";
+import {useSelector} from "react-redux";
 import {AppRootStateType} from "../store/store";
-import {addWordAC, WordsStateType} from "../store/words-store";
-import {useInput} from "../utils/utils";
+import {WordsStateType} from "../store/words-store";
 
 const AddWordsPage = () => {
-    //using custom hook for handling inputs
-    const rus = useInput('');
-    const eng = useInput('');
-
     const words = useSelector<AppRootStateType, WordsStateType>(state => state.words);
-    const dispatch = useDispatch();
-
-    function onSubmit() {
-        dispatch(addWordAC(rus.value, eng.value));
-        rus.setValue('');
-        eng.setValue('');
-        console.log('Rus:', rus.value, 'Eng:', eng.value);
-    }
-
     return (
         <Container>
-            <AddWordForm
-                rus={rus.value}
-                eng={eng.value}
-                errMessRus={rus.error}
-                errMessEng={eng.error}
-                rusInputOnChangeHandler={rus.onChange}
-                engInputOnChangeHandler={eng.onChange}
-                onSubmit={onSubmit}
-            />
+            <AddWordForm/>
             <WordTable
                 words={words}
             />
