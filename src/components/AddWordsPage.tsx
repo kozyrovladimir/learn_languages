e2aa406f22510e78d431e7c30611c7b3c2071584
kozyrovladimir <kozyrovladimir@gmail.com>
2022-07-04 15:09:17 +0300

    feature: create form for translate words via external api

--- src/components/AddWordsPage.tsx
+++ src/components/AddWordsPage.tsx
@@ -3,12 +3,14 @@ import AddWordForm from "./AddWordForm";
 import {Container} from "@mui/material";
 import WordTable from './WordTable';
 import {useAppSelector} from "../hooks/redux";
+import AddWordFromApi from "./AddWordFromAPI";
 
 const AddWordsPage = () => {
     const words = useAppSelector(state => state.wordsReducer)
     return (
         <Container>
             <AddWordForm/>
+            <AddWordFromApi/>
             <WordTable
                 words={words}
             />
