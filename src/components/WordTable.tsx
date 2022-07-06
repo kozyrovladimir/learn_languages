590b86e156f51b014fa5d7253ec327c9f898b68c
kozyrovladimir <kozyrovladimir@gmail.com>
2022-07-06 15:37:16 +0300

    fix: add paddings.

--- src/components/WordTable.tsx
+++ src/components/WordTable.tsx
@@ -10,7 +10,7 @@ import {wordsSlice, WordsStateType} from "../store/words-store";
 import {useAppDispatch} from "../hooks/redux";
 import StarRating from "./StarRating";
 import Typography from '@mui/material/Typography';
-import {IconButton, Stack} from "@mui/material";
+import {Box, IconButton, Stack} from "@mui/material";
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 import DoneIcon from '@mui/icons-material/Done';
@@ -26,100 +26,102 @@ export default function WordTable(props: WordTablePropsType) {
     const {changeRatingWord} = wordsSlice.actions;
 
     return (
-        <TableContainer component={Paper}>
-            <Table sx={{minWidth: 650}} aria-label="simple table">
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Rus:</TableCell>
-                        <TableCell>Eng:</TableCell>
-                        <TableCell>Прогресс изучения:</TableCell>
-                        <TableCell>Дата добавления:</TableCell>
-                        <TableCell>Действия:</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {props.words.map((word) => {
-                        const date = new Date(word.date);
-                        const wordColor = word.rating === 3 ? '#00d700' : '';
-                        const backgroundColorRow = word.rating === 3 ? '#f6fff6' : '';
+        <Box sx={{paddingTop: 2, paddingBottom: 2}}>
+            <TableContainer component={Paper}>
+                <Table sx={{minWidth: 650}} aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Rus:</TableCell>
+                            <TableCell>Eng:</TableCell>
+                            <TableCell>Прогресс изучения:</TableCell>
+                            <TableCell>Дата добавления:</TableCell>
+                            <TableCell>Действия:</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {props.words.map((word) => {
+                            const date = new Date(word.date);
+                            const wordColor = word.rating === 3 ? '#00d700' : '';
+                            const backgroundColorRow = word.rating === 3 ? '#f6fff6' : '';
 
-                        return (
-                            <TableRow
-                                key={word.id}
-                                sx={{
-                                    '&:last-child td, &:last-child th': {border: 0},
-                                    backgroundColor: backgroundColorRow,
-                                    '&:hover': {backgroundColor: 'rgba(255,196,0,0.13)',
-                                    transition: 'all 100ms ease-in-out'}
-                                }}
-                            >
-                                <TableCell component="th" scope="row">
-                                    <Typography sx={{color: wordColor}}>
-                                        {word.rus}
-                                    </Typography>
-                                </TableCell>
-                                <TableCell component="th" scope="row">
-                                    <Typography sx={{color: wordColor}}>
-                                        {word.eng}
-                                    </Typography>
-                                </TableCell>
-                                <TableCell>
-                                    <StarRating starAmount={word.rating}/>
-                                </TableCell>
-                                <TableCell>{date.toLocaleDateString()} {date.toLocaleTimeString()}</TableCell>
-                                <TableCell sx={{display: 'flex', flexDirection: 'column'}}>
-                                    {/*<Box>*/}
-                                    <Stack direction="row" spacing={1} justifyContent="center">
-                                        <IconButton aria-label="edit">
-                                            <EditIcon color={'action'}/>
-                                        </IconButton>
-                                        <IconButton
-                                            disabled={word.rating === 3}
-                                            onClick={() => {
-                                                dispatch(changeRatingWord({id: word.id, newRating: 3}))
-                                            }}
-                                            aria-label="edit">
-                                            <DoneIcon color={word.rating === 3 ? 'disabled' :'success'}/>
-                                        </IconButton>
-                                        <IconButton
-                                            disabled={word.rating === 0}
-                                            onClick={() => {
-                                                dispatch(changeRatingWord({id: word.id, newRating: 0}))
-                                            }}
-                                            aria-label="edit"
-                                        >
-                                            <ReplayIcon color={word.rating === 0 ? 'disabled' :'warning'}/>
-                                        </IconButton>
-                                        <IconButton aria-label="delete">
-                                            <DeleteIcon color={'error'}/>
-                                        </IconButton>
-                                    </Stack>
-                                    {/*</Box>*/}
-                                    {/*<Button*/}
-                                    {/*    onClick={() => {*/}
-                                    {/*        dispatch(changeRatingWord({id: word.id, newRating: 0}));*/}
-                                    {/*    }}*/}
-                                    {/*    disabled={word.rating === 0}*/}
-                                    {/*    color={"error"}*/}
-                                    {/*    sx={{marginBottom: 1}}*/}
-                                    {/*    variant={'outlined'}*/}
-                                    {/*    size={'small'}*/}
-                                    {/*>изучать заново</Button>*/}
-                                    {/*<Button*/}
-                                    {/*    onClick={() => {*/}
-                                    {/*        dispatch(changeRatingWord({id: word.id, newRating: 3}));*/}
-                                    {/*    }}*/}
-                                    {/*    disabled={word.rating === 3}*/}
-                                    {/*    color={"success"}*/}
-                                    {/*    variant={'outlined'}*/}
-                                    {/*    size={'small'}*/}
-                                    {/*>в изученные</Button>*/}
-                                </TableCell>
-                            </TableRow>
-                        )
-                    })}
-                </TableBody>
-            </Table>
-        </TableContainer>
+                            return (
+                                <TableRow
+                                    key={word.id}
+                                    sx={{
+                                        '&:last-child td, &:last-child th': {border: 0},
+                                        backgroundColor: backgroundColorRow,
+                                        '&:hover': {backgroundColor: 'rgba(255,196,0,0.13)',
+                                        transition: 'all 100ms ease-in-out'},
+                                    }}
+                                >
+                                    <TableCell component="th" scope="row">
+                                        <Typography sx={{color: wordColor}}>
+                                            {word.rus}
+                                        </Typography>
+                                    </TableCell>
+                                    <TableCell component="th" scope="row">
+                                        <Typography sx={{color: wordColor}}>
+                                            {word.eng}
+                                        </Typography>
+                                    </TableCell>
+                                    <TableCell>
+                                        <StarRating starAmount={word.rating}/>
+                                    </TableCell>
+                                    <TableCell>{date.toLocaleDateString()} {date.toLocaleTimeString()}</TableCell>
+                                    <TableCell sx={{display: 'flex', flexDirection: 'column'}}>
+                                        {/*<Box>*/}
+                                        <Stack direction="row" spacing={1} justifyContent="center">
+                                            <IconButton aria-label="edit">
+                                                <EditIcon color={'action'}/>
+                                            </IconButton>
+                                            <IconButton
+                                                disabled={word.rating === 3}
+                                                onClick={() => {
+                                                    dispatch(changeRatingWord({id: word.id, newRating: 3}))
+                                                }}
+                                                aria-label="edit">
+                                                <DoneIcon color={word.rating === 3 ? 'disabled' :'success'}/>
+                                            </IconButton>
+                                            <IconButton
+                                                disabled={word.rating === 0}
+                                                onClick={() => {
+                                                    dispatch(changeRatingWord({id: word.id, newRating: 0}))
+                                                }}
+                                                aria-label="edit"
+                                            >
+                                                <ReplayIcon color={word.rating === 0 ? 'disabled' :'warning'}/>
+                                            </IconButton>
+                                            <IconButton aria-label="delete">
+                                                <DeleteIcon color={'error'}/>
+                                            </IconButton>
+                                        </Stack>
+                                        {/*</Box>*/}
+                                        {/*<Button*/}
+                                        {/*    onClick={() => {*/}
+                                        {/*        dispatch(changeRatingWord({id: word.id, newRating: 0}));*/}
+                                        {/*    }}*/}
+                                        {/*    disabled={word.rating === 0}*/}
+                                        {/*    color={"error"}*/}
+                                        {/*    sx={{marginBottom: 1}}*/}
+                                        {/*    variant={'outlined'}*/}
+                                        {/*    size={'small'}*/}
+                                        {/*>изучать заново</Button>*/}
+                                        {/*<Button*/}
+                                        {/*    onClick={() => {*/}
+                                        {/*        dispatch(changeRatingWord({id: word.id, newRating: 3}));*/}
+                                        {/*    }}*/}
+                                        {/*    disabled={word.rating === 3}*/}
+                                        {/*    color={"success"}*/}
+                                        {/*    variant={'outlined'}*/}
+                                        {/*    size={'small'}*/}
+                                        {/*>в изученные</Button>*/}
+                                    </TableCell>
+                                </TableRow>
+                            )
+                        })}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </Box>
     );
 }
