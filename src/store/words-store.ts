a1dcb3a8f2422263f1bf9dfcb26d51398ff7a5be
kozyrovladimir <kozyrovladimir@gmail.com>
2022-07-18 17:16:02 +0300

    feature: add initial state

--- src/store/words-store.ts
+++ src/store/words-store.ts
@@ -16,7 +16,24 @@ export type WordType = {
 export type WordsStateType = Array<WordType>;
 
 //initial state type
-const initialState: Array<WordType> = [];
+const initialState: Array<WordType> = [
+    {rus: 'голова', eng: 'head', date: new Date(), rating: 0, id: v1()},
+    {rus: 'брать', eng: 'take', date: new Date(), rating: 0, id: v1()},
+    {rus: 'мужчина', eng: 'man', date: new Date(), rating: 0, id: v1()},
+    {rus: 'думать', eng: 'think', date: new Date(), rating: 0, id: v1()},
+    {rus: 'низкий', eng: 'low', date: new Date(), rating: 0, id: v1()},
+    {rus: 'когда', eng: 'when', date: new Date(), rating: 0, id: v1()},
+    {rus: 'много', eng: 'many', date: new Date(), rating: 0, id: v1()},
+    {rus: 'идти', eng: 'go', date: new Date(), rating: 0, id: v1()},
+    {rus: 'сейчас', eng: 'now', date: new Date(), rating: 0, id: v1()},
+    {rus: 'чай', eng: 'tea', date: new Date(), rating: 0, id: v1()},
+    {rus: 'еда', eng: 'food', date: new Date(), rating: 0, id: v1()},
+    {rus: 'тяжелый', eng: 'hard', date: new Date(), rating: 0, id: v1()},
+    {rus: 'однажды', eng: 'once', date: new Date(), rating: 0, id: v1()},
+    {rus: 'дети', eng: 'children', date: new Date(), rating: 0, id: v1()},
+    {rus: 'пример', eng: 'example', date: new Date(), rating: 0, id: v1()},
+    {rus: 'говорить', eng: 'talk', date: new Date(), rating: 0, id: v1()},
+];
 
 //redux toolkit
 
