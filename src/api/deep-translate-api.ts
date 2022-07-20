f2d7e614c150f92554ad3172213d9efa0f22ba06
kozyrovladimir <kozyrovladimir@gmail.com>
2022-07-20 15:31:48 +0300

    refactor: moving api constants to env file

--- src/api/deep-translate-api.ts
+++ src/api/deep-translate-api.ts
@@ -1,18 +1,19 @@
 import axios from "axios";
+import {API_URL, API_SUB_URL, API_HOST, API_KEY} from "../constants/api";
 
 const instance = axios.create({
-    baseURL: 'https://deep-translate1.p.rapidapi.com',
+    baseURL: API_URL,
     headers: {
-        'X-RapidAPI-Key': '5cc0866329msh8b6169514926715p1cb860jsn9771154bd221',
-        'X-RapidAPI-Host': 'deep-translate1.p.rapidapi.com'
+        'X-RapidAPI-Key': API_KEY,
+        'X-RapidAPI-Host': API_HOST
     }
 });
 
 export const deepTranslateAPI = {
     translateRuToEn(text: string) {
-       return  instance.post('/language/translate/v2',`{"q":"${text}","source":"ru","target":"en"}`);
+        return instance.post(API_SUB_URL, `{"q":"${text}","source":"ru","target":"en"}`);
     },
     translateEnToRu(text: string) {
-        return  instance.post('/language/translate/v2',`{"q":"${text}","source":"en","target":"ru"}`);
+        return instance.post(API_SUB_URL, `{"q":"${text}","source":"en","target":"ru"}`);
     },
 }
