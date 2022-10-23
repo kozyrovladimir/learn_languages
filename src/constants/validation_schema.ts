35d540f6f45bdb36ed19abcf7f00294491d2c992
kozyrovladimir <kozyrovladimir@gmail.com>
2022-10-23 17:23:10 +0300

    feature: check if word exists in storage before adding

--- src/constants/validation_schema.ts
+++ src/constants/validation_schema.ts
@@ -1,24 +1,38 @@
 import * as yup from 'yup';
 
+import { useEngWords, useRusWords } from '../hooks/words';
+
 const maxSymbolsValue = 20;
 
-export const validationSchemaRusEng = yup.object({
-  rus: yup
-    .string()
-    .max(maxSymbolsValue, 'Максимум 20 символов!')
-    .required('Поле обязательно!')
-    .trim(),
-  eng: yup
-    .string()
-    .max(maxSymbolsValue, 'Максимум 20 символов!')
-    .required('Поле обязательно!')
-    .trim(),
-});
+export const useValidationSchemaRusEng = (): any => {
+  const rusWords = useRusWords();
+  const engWord = useEngWords();
 
-export const validationSchemaTranslate = yup.object({
-  translateWord: yup
-    .string()
-    .max(maxSymbolsValue, 'Не больше 20 символов!')
-    .required('Поле обязательно!')
-    .trim(),
-});
+  return yup.object({
+    rus: yup
+      .string()
+      .max(maxSymbolsValue, 'Максимум 20 символов!')
+      .required('Поле обязательно!')
+      .trim()
+      .notOneOf(rusWords, 'Слово уже есть в хранилище!'),
+    eng: yup
+      .string()
+      .max(maxSymbolsValue, 'Максимум 20 символов!')
+      .required('Поле обязательно!')
+      .trim()
+      .notOneOf(engWord, 'Слово уже есть в хранилище!'),
+  });
+};
+
+export const useValidationSchemaTranslate = (): any => {
+  const rusWords = useRusWords();
+
+  return yup.object({
+    translateWord: yup
+      .string()
+      .max(maxSymbolsValue, 'Не больше 20 символов!')
+      .required('Поле обязательно!')
+      .trim()
+      .notOneOf(rusWords, 'Слово уже есть в хранилище!'),
+  });
+};
